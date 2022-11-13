ff9b8e277fe90d9772d6fd6ec095591df7d95d9f
FatlindBytyqi <fb52623@ubt-uni.net>
2022-11-14 00:08:30 +0100

    First Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Portfolio_FatlindBytyqi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
