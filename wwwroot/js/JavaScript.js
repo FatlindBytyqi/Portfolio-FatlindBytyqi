945692fe1fa4fa5ee72ac5811dfbbe95c25996a4
FatlindBytyqi <fb52623@ubt-uni.net>
2022-11-14 01:13:46 +0100

    Added styling to second slideshow container, and the logic to it, PS-Forgot to point out that i've linked style.css in index.html external way and some <script> tags with buttons logic

--- wwwroot/js/JavaScript.js
+++ wwwroot/js/JavaScript.js
@@ -21,3 +21,27 @@ function showSlides(n) {
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " active";
 }
+
+var slideIndex1 = 1;
+showSlides1(slideIndex1);
+function plusSlides1(m) {
+    showSlides1(slideIndex1 += m);
+}
+function currentSlide1(n) {
+    showSlides1(slideIndex1 = m);
+}
+function showSlides1(m) {
+    var x;
+    var slides1 = document.getElementsByClassName("mySlides1");
+    var dots1 = document.getElementsByClassName("dot1");
+    if (m > slides1.length) { slideIndex1 = 1 }
+    if (m < 1) { slideIndex1 = slides1.length }
+    for (x = 0; x < slides1.length; x++) {
+        slides1[x].style.display = "none";
+    }
+    for (x = 0; x < dots1.length; x++) {
+        dots1[x].className = dots1[x].className.replace(" active", "");
+    }
+    slides1[slideIndex1 - 1].style.display = "block";
+    dots1[slideIndex1 - 1].className += " active";
+}
